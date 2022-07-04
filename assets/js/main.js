07c243b3e407af45466c34b7709557ac2dabd1ff
Tee Chor Yang <cyang0330@gmail.com>
2022-07-05 01:44:32 +0800

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+document.addEventListener("DOMContentLoaded", function(event) { 
 (function() {
   "use strict";
 
@@ -294,3 +295,4 @@
   });
 
 })()
+});
